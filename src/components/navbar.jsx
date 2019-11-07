89771d72ea29986401c931f8b4e172660175049b
elenichar77 <charitoue@ece.auth.gr>
2019-11-07 14:21:55 +0200

    Fix navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,29 +3,70 @@ import React, { Component } from "react";
 class NavBar extends Component {
   render() {
     return(
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+/*{ <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  <a href="/house" className="nav-brand"><img src="cat.png" alt="logo" width="80%" ></img></a>
 
   <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
     <ul className="navbar-nav mr-auto mt-2 mt-md-0">
+      
+     <div class="container"> 
       <li className="nav-item active">
-        <a className="nav-link" href="/rent"><h3>Rent</h3>  <span className="sr-only">(current)</span></a>
+      <div className="item item-1"><a className="nav-link" href="/rent">Rent<span className="sr-only">(current)</span></a></div>
       </li>
       <li className="nav-item active">
-      <a className="nav-link" href="/buy"><h3>Buy</h3>  <span className="sr-only">(current)</span></a>
+      <div className="item item-1"><a className="nav-link" href="/buy">Buy<span className="sr-only">(current)</span></a></div>
       </li>
       <li className="nav-item active">
-        <a className="nav-link" href="/about"><h3>About</h3>  <span className="sr-only">(current)</span></a>
+      <div className="item item-2"><a href="/" className="nav-brand"><img src="#" alt="logo" width="20%" ></img></a></div>
       </li>
       <li className="nav-item active">
-        <a className="nav-link" href="/add"><h3>Add house</h3>  <span className="sr-only">(current)</span></a>
+      <div className="item item-1"><a className="nav-link" href="/about">About<span className="sr-only">(current)</span></a></div>
       </li>
-
+      <li className="nav-item active">
+      <div className="item item-1"><a className="nav-link" href="/add">Add house<span className="sr-only">(current)</span></a></div>
+      </li>
+    </div>
     </ul>
   </div>
+</nav> }*/
+
+
+
+<nav className="navbar fixed-top navbar-expand-lg navbar white scrolling-navbar">
+<a className="navbar-brand" href=""><img src="#" alt="logo"/></a>
+<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span className="navbar-toggler-icon"></span>
+</button>
+<div className="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul className="navbar-nav mr-auto">
+    <li className="nav-item active">
+    <a className="nav-link" href="/rent">Rent</a>
+    </li>
+    <li className="nav-item">
+    <a className="nav-link" href="/buy">Buy</a>
+    </li>
+    <li className="nav-item">
+    <a className="nav-link" href="/about">About</a>
+    </li>
+    <li classn="nav-item">
+      <a className="nav-link" href="/add">Add house</a>
+    </li>
+  </ul>
+  <ul className="navbar-nav nav-flex-icons">
+    <li className="nav-item">
+      <a className="nav-link"><i className="fab fa-facebook-f"></i></a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link"><i className="fab fa-twitter"></i></a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link"><i className="fab fa-instagram"></i></a>
+    </li>
+  </ul>
+</div>
 </nav>
 
 // <nav className="navbar navbar-default" role="navigation">
