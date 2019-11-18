4d2a3424400a4e63f9ba595a311e2d1c7c64ca98
olyxen <dai16058@uom.edu.gr>
2019-11-19 01:49:57 +0200

    Set values to lastHouses items

--- src/components/lastHouses.jsx
+++ src/components/lastHouses.jsx
@@ -1,73 +1,60 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class LastHouses extends Component {
-  state = {};
+  
+  state = {
+    houses: []
+  };
+
+  componentDidMount(){
+    
+      axios.get(`https://housecat-skgcode-api.herokuapp.com/api/3properties`).then(res => {
+        const houses = res.data;
+        this.setState({ houses });
+    });
+    
+  }
+
   render() {
     return (
       <React.Fragment>
-        <div className="d-flex justify-content-center ">
-          <h1 class="display-4 lastHouseText">Our last entries</h1>
-        </div>
-        <div className="d-flex flex-row justify-content-center lastHousesBox">
-          <div className="col-md-2">
-            <div class="card mb-2">
-              <img
-                class="card-img-top"
-                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
-                alt="Card image cap"
-              />
-              <div class="card-body">
-                <h4 class="card-title d-flex justify-content-center">
-                  Card title
-                </h4>
-                <p id="about" class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a class="btn btn-info d-flex justify-content-center">Button</a>
+          <div className="d-flex justify-content-center ">
+            <h1 class="display-4 lastHouseText">Our last entries</h1>
+          </div>
+          <br/>
+          <div className="d-flex flex-row justify-content-center lastHousesBox">
+          {this.state.houses.map(house => (
+            <div className="col-md-2" key={house.id}>
+              <div class="card mb-2">
+                <img
+                  class="card-img-top"
+                  src={house.img_url}
+                  alt="Card image cap"
+                  height="200px"
+                />
+                <div class="card-body">
+                  <h4 class="card-title d-flex justify-content-center textcard">
+                  {house.property_type_title} at {house.location}
+                  </h4>
+                  <p id="about" class="card-text">
+                  <ul className="houseDetails"> 
+                   <li><strong>Sqm:</strong> {house.sqm} m²</li>
+                   <li><strong>Price:</strong> {house.price} €</li>
+                   <li><strong>For:</strong> {house.sale_type_title}</li>
+                 </ul>
+                  </p>
+                  {/* We have to add redirect to detail view page on this button */}
+                  <a class="btn btn-info d-flex justify-content-center">More details</a>
+                </div>
               </div>
             </div>
+            ))}
           </div>
-          <div className="col-md-2">
-            <div class="card mb-2 ">
-              <img
-                class="card-img-top"
-                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
-                alt="Card image cap"
-              />
-              <div class="card-body">
-                <h4 class="card-title d-flex justify-content-center">
-                  Card title
-                </h4>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a class="btn btn-info d-flex justify-content-center">Button</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-2">
-            <div class="card mb-2 ">
-              <img
-                class="card-img-top"
-                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
-                alt="Card image cap"
-              />
-              <div class="card-body">
-                <h4 class="card-title d-flex justify-content-center">
-                  Card title
-                </h4>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a class="btn btn-info d-flex justify-content-center">Button</a>
-              </div>
-            </div>
-          </div>
-        </div>
+          
       </React.Fragment>
+
+      
     );
   }
 }
