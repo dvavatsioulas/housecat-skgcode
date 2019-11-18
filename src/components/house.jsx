d34e53ed243628f5c49d936c0347ad9067ccd7b5
olyxen <dai16058@uom.edu.gr>
2019-11-18 16:25:48 +0200

    Fix some design problems

--- src/components/house.jsx
+++ src/components/house.jsx
@@ -11,40 +11,78 @@ class House extends Component {
   }
 
   render() {
-    return (
-      <ul className="houses">
-        {this.state.houses.map(house => (
-          <li className="block-example" key={house.id}>
+    // return (
+    //   <ul className="houses">
+    //     {this.state.houses.map(house => (
+    //       <li className="block-example" key={house.id}>
+    //         <div>
+    //           <img
+    //             src={house.img_url}
+    //             alt="house image"
+    //             width="100%"
+    //             height="100%"
+    //           ></img>
+    //         </div>
+    //         <tr>
+    //           <td>{house.sqm} m²</td>
+    //           <td>{house.price} €</td>
+    //           <td>{house.location}</td>
+    //         </tr>
+    //         <tr>
+    //           <td>bedrooms: {house.bedrooms}</td>
+    //           <td>bathrooms: {house.bathrooms}</td>
+    //           <td>floor: {house.floor}</td>
+    //         </tr>
+    //         <tr>
+    //           <td>heating: {house.heating_type}</td>
+    //           <td>built: {house.built_year}</td>
+    //         </tr>
+    //         <tr>
+    //           <td>phone: {house.phone}</td>
+    //           <td>email: {house.email}</td>
+    //         </tr>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // );
+    return(
+      <ul class="container-fluid house">
+         {this.state.houses.map(house => (
+          <li class="block-example" key={house.id}>
             <div>
-              <img
-                src={house.img_url}
-                alt="house image"
-                width="100%"
-                height="100%"
-              ></img>
+              <div class="card-group row">
+                <div class="col-md-4">
+                  <img src={house.img_url} class="card-img img-fluid img-thumbnail" alt="house image"></img>
+                </div>
+                <div class="col-md-7">
+                  <div class="card-body">
+                  <tr>
+                   <td>{house.sqm} m²</td>
+                   <td>{house.price} €</td>
+                   <td>{house.location}</td>
+                 </tr>
+                 <tr>
+                   <td>bedrooms: {house.bedrooms}</td>
+                   <td>bathrooms: {house.bathrooms}</td>
+                   <td>floor: {house.floor}</td>
+                 </tr>
+                 <tr>
+                   <td>heating: {house.heating_type}</td>
+                   <td>built: {house.built_year}</td>
+                 </tr>
+                 <tr>
+                   <td>phone: {house.phone}</td>
+                   <td>email: {house.email}</td>
+                 </tr>
+                  </div>
+                </div>
+              </div>
             </div>
-            <tr>
-              <td>{house.sqm} m²</td>
-              <td>{house.price} €</td>
-              <td>{house.location}</td>
-            </tr>
-            <tr>
-              <td>bedrooms: {house.bedrooms}</td>
-              <td>bathrooms: {house.bathrooms}</td>
-              <td>floor: {house.floor}</td>
-            </tr>
-            <tr>
-              <td>heating: {house.heating_type}</td>
-              <td>built: {house.built_year}</td>
-            </tr>
-            <tr>
-              <td>phone: {house.phone}</td>
-              <td>email: {house.email}</td>
-            </tr>
           </li>
         ))}
       </ul>
-    );
+
+  );
   }
 }
 
