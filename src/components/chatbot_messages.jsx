54024669253f4041ed9bf217c093fb361d302f96
olyxen <dai16058@uom.edu.gr>
2019-11-21 23:47:46 +0200

    Change the color of chatbot

--- src/components/chatbot_messages.jsx
+++ src/components/chatbot_messages.jsx
@@ -6,13 +6,13 @@ const Message = (props)=> {
         <div class="container-fluid" >
 
                     {props.speaks==='bot' && 
-                    <div className="card bg-info"  style={{position:"left", display:"flex", margin: 0, paddingBottom: '5%', paddingTop: '5%', height: '90%', textAlign: 'auto',  display:"flex",justifyContent: "center", alignItems: "center"}}>
+                    <div className="card"  style={{position:"left", display:"flex",margin: 0, paddingBottom: '1%', paddingTop: '1%', marginBottom:'2%',  textAlign: 'auto',  display:"flex",justifyContent: "center", alignItems: "center",backgroundColor:'#00b3b3'}}>
 
                         <div className="container" style={{alignSelf: 'stretch'}}>
-                            <div className="col-sm-3"  style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                            <div className="col-sm-3"  style={{display: "flex", justifyContent: "center", alignItems: "center", left:0, backgroundColor:'#00b3b3'}}>
                                 <h5 style={{maxHeight:22.5, minHeight:22.5}}><span className="badge badge-light" style={{display:"static" , minWidth: 40, verticalAlign:"middle"}}>{props.speaks}</span></h5>
                             </div>
-                            <div className="col-sm-9" style={{paddingBottom: '2%', paddingTop: '2%', height: '96%', justifyContent: "center"}}>
+                            <div className="col-sm-9" style={{paddingBottom: '2%', paddingTop: '2%', justifyContent: "center",backgroundColor:'#00b3b3'}}>
                                 <div className="text-white" style={{paddingBottom: '2%', paddingTop: '2%', height: '96%', }}>
                                     {props.text}
                                 </div>
