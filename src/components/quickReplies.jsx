88e07689a2bde9b5eb0f326dd887e9cb46cc411e
kostaskam <kkaamper@gmail.com>
2019-12-04 02:04:43 +0200

    correction of chatbot buttons (make them like the rest conversation)

--- src/components/quickReplies.jsx
+++ src/components/quickReplies.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import QuickReply from './quickReply';
+import "../message.css";
+import "../chat-window.css";
 
 class QuickReplies extends Component {
     constructor(props) {
@@ -28,24 +30,15 @@ class QuickReplies extends Component {
 
     render() {
         return (
-            <div class="container-fluid" >
-
-                    <div className="card"  style={{position:"left", display:"flex",margin: 0, paddingBottom: '1%', paddingTop: '1%', marginBottom:'2%',  textAlign: 'auto',  display:"flex",justifyContent: "center", alignItems: "center",backgroundColor:'#00b3b3'}}>
-
-                        <div className="container" style={{alignSelf: 'stretch'}}>
-                            <div className="col-sm-3"  style={{display: "flex", justifyContent: "center", alignItems: "center", left:0, backgroundColor:'#00b3b3'}}>
-                                <h5 style={{maxHeight:22.5, minHeight:22.5}}><span className="badge badge-light" style={{display:"static" , minWidth: 40, verticalAlign:"middle"}}>{this.props.speaks}</span></h5>
-                            </div>
-                            <div id= "quick-replies" className="col-sm-9" style={{paddingBottom: '2%', paddingTop: '2%', justifyContent: "center",backgroundColor:'#00b3b3'}}>
-                                <div className="text-white" style={{paddingBottom: '2%', paddingTop: '2%', height: '96%', }}>
-                                    {this.props.text && <p>{this.props.text.stringValue} </p>}
-                                    {this.renderQuickReplies(this.props.payload)}
-                                </div>
-                            </div>
-                        </div>
+            <div class="sc-message" style={{marginLeft: '20px', marginRight: '0px', padding: '0px'}}>
+                <div className="sc-message--content received">
+                    <div className="sc-message--avatar" style={{backgroundImage: "url(https://yt3.ggpht.com/a/AGF-l7_5_gsypJpmcfZFJlo7QlWEGHixtwrEuzgEaw=s900-c-k-c0xffffffff-no-rj-mo)"}}></div>
+                    <div className="sc-message--text">
+                                {this.props.text && <p>{this.props.text.stringValue} </p>}
+                                {this.renderQuickReplies(this.props.payload)}
                     </div>
-                
                 </div>
+            </div>
         );
     }
 }
