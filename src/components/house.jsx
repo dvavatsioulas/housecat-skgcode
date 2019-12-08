2f0b0138685cc5980431ec58d8226f19e0153695
olyxen <dai16058@uom.edu.gr>
2019-12-08 21:42:20 +0200

    Improve the appearance of result list

--- src/components/house.jsx
+++ src/components/house.jsx
@@ -26,35 +26,34 @@ class House extends Component {
       );
     } else {
       return (
-        <div className="d-flex p-8 bd-highlight housesDiv">
-          <ul class="houses">
+        <div className="d-flex p-8 bd-highlight">
+          <ul className="houses">
             {this.state.houses.map(house => (
               <li class="block-example house" key={house.id}>
-                <div>
+                
                   <div class="card-group row ">
                     <div class="col-md-4">
                       <Link to={`/houses/${house.id}`} >
-                  <img
-                    class="card-img img-fluid house-img"
-                    src={house.img_url}
-                    alt="Card image cap"
-                    height="150px"
-                  />
-                </Link>
+                        <img
+                          class="card-img img-fluid house-img"
+                          src={house.img_url}
+                          alt="Card image cap"
+                        />
+                      </Link>
                     </div>
-                    <div class="col-md-8 ">
+                    <div class="col-md-7">
                       <div class="card-body houseDetails">
                         <p id="housetitle">{ house.property_type +" "+ house.sqm +"m² for " + house.sale_type}</p>
                         <p class="fas fa-map-marker-alt" style={{fontSize:'24px',fontStyle:'initial'}}> {house.location} {house.price}€</p>
-                        <p style={{fontSize:'17px'}}>{house.description}</p>
+                        <p id="houseDesc">{house.description}</p>
                         <ul className="row">
-                          <li style={{marginRight:'30px',fontSize:'17px'}}>Bedrooms: {house.bedrooms}</li>
-                          <li style={{fontSize:'17px'}}>Floor: {house.floor}</li>
+                          <li style={{marginRight:'30px',fontSize:'17px',fontFamily:"georgia"}}>Bedrooms: {house.bedrooms}</li>
+                          <li style={{fontSize:'17px',fontFamily:"georgia"}}>Floor: {house.floor}</li>
                         </ul>
-                        <Link to={`/houses/${house.id}`} class="btn d-flex justify-content-center float-right" style={{width:'30%', marginBottom:'2%', backgroundColor :"#2ABBAC", color: "white"}}>More details</Link>
+                        <Link to={`/houses/${house.id}`} class="btn d-flex justify-content-center float-right" style={{width:'30%', marginBottom:'2%', backgroundColor :"#2ABBAC", color: "white", bottom:0, marginLeft:"60%", position:"absolute"}}>More details</Link>
                       </div>
                     </div>
-                  </div>
+                  
                 </div>
               </li>
             ))}
