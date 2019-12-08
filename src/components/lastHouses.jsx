7374bfb412ec7a7adeae37910a8a0bf42047610a
olyxen <dai16058@uom.edu.gr>
2019-12-09 00:25:59 +0200

    Make "lastHouses" flexible

--- src/components/lastHouses.jsx
+++ src/components/lastHouses.jsx
@@ -23,39 +23,29 @@ class LastHouses extends Component {
           <h1 class="display-4 lastHouseText">Our last entries</h1>
         </div>
         <br />
-        <div className="d-flex flex-row justify-content-center lastHousesBox">
+        <div class="container justify-content-center lastHousesBox">
+          <div class="card-deck">
           {this.state.houses.map(house => (
-            <div className="col-md-2" key={house.id}>
-              <div class="card mb-2" style={{ minWidth: 130 }}>
-                <Link to={`/houses/${house.id}`} >
-                  <img
-                    class="card-img-top"
-                    src={house.img_url}
-                    alt="Card image cap"
-                    height="200px"
-                  />
-                </Link>
-
-                <div class="card-body">
-                  <h4 class="card-title d-flex justify-content-center">
-                    {house.property_type} at {house.location}
-                  </h4>
-                  <p id="about" class="card-text">
-                    <ul className="house3Details">
-                      <li><strong>Sqm:</strong> {house.sqm} m²</li>
-                      <li><strong>Price:</strong> {house.price} €</li>
-                      <li><strong>For:</strong> {house.sale_type}</li>
-                    </ul>
-                  </p>
-
-                  <Link to={`/houses/${house.id}`} class="btn btn-info d-flex justify-content-center">More details</Link>
-                </div>
+              <div class="card mb-2 col-md-3.5" key={house.id}>
+                  <Link to={`/houses/${house.id}`} >
+                    <img id="lastHousesImg"class="card-img-top img-fluid" src={house.img_url} alt="Card image cap"></img>
+                    </Link>
+                  <div class="card-body">
+                      <h4 class="card-title">{house.property_type} at {house.location}</h4>
+                      <p id="about" class="card-text">
+                        <ul className="house3Details">
+                          <li><strong>Sqm:</strong> {house.sqm} m²</li>
+                          <li><strong>Price:</strong> {house.price} €</li>
+                          <li><strong>For:</strong> {house.sale_type}</li>
+                        </ul>
+                      </p>
+                      <p class="card-text"><Link to={`/houses/${house.id}`} class="btn btn-info d-flex justify-content-center">More details</Link></p>
+                  </div>
               </div>
-            </div>
-          ))
-          }
-        </div>
-
+              ))
+            }
+            </div> 
+          </div>
       </React.Fragment >
     );
   }
