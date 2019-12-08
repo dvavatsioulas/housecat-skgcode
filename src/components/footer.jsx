303c7ac08afd90bca31d5b39f127e91530740219
olyxen <dai16058@uom.edu.gr>
2019-12-08 21:39:08 +0200

    Remove all the unnecessary divs and align icons to the center

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -18,31 +18,24 @@ class Footer extends Component {
   state = {};
   render() {
     return (
-      <footer className="page-footer bottom font-small footer-dark black pt-4">
-        <div className="container-fluid text-center text-md-left">
-            <hr className="clearfix w-100 d-md-none pb-3" />
-
-            <div className="col-md-6 mt-md-0 mt-3">
-              <h5 className="text-uppercase" style={styleText}>
-                Connect with us:
-              </h5>
-
-              <MDBContainer>
-                <MDBBtn size="xlg" tag="a" floating social="fb" target="_blank" href="https://www.facebook.com/skgcode/">
-                  <MDBIcon fab icon="facebook-f" />
-                </MDBBtn>
-                <MDBBtn size="xlg" tag="a" floating social="tw">
-                  <MDBIcon fab icon="twitter" />
-                </MDBBtn>
-                <MDBBtn size="xlg" tag="a" floating social="gplus">
-                  <MDBIcon fab icon="google-plus-g" />
-                </MDBBtn>
-                <MDBBtn size="xlg" tag="a" floating social="li" target="_blank" href="https://www.linkedin.com/company/skgcode/">
-                  <MDBIcon fab icon="linkedin-in" />
-                </MDBBtn>
-              </MDBContainer>
-            </div>
-          </div>
+      <footer className="page-footer font-small footer-dark black pt-4">
+        <h5 className="text-uppercase text-center" >
+          Connect with us:
+        </h5>
+        <MDBContainer className="float-center">
+          <MDBBtn size="xlg" tag="a" floating social="fb" target="_blank" href="https://www.facebook.com/skgcode/" style={{marginLeft:"34.3%"}}>
+            <MDBIcon fab icon="facebook-f" />
+          </MDBBtn>
+          <MDBBtn size="xlg" tag="a" floating social="tw">
+            <MDBIcon fab icon="twitter" />
+          </MDBBtn>
+          <MDBBtn size="xlg" tag="a" floating social="gplus">
+            <MDBIcon fab icon="google-plus-g" />
+          </MDBBtn>
+          <MDBBtn size="xlg" tag="a" floating social="li" target="_blank" href="https://www.linkedin.com/company/skgcode/">
+            <MDBIcon fab icon="linkedin-in" />
+          </MDBBtn>
+        </MDBContainer>
         <div
           className="footer-copyright text-center footer-dark blacks py-3"
           style={styleText}
