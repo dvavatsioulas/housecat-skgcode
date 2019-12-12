b4823f7f6d15c57d1426a3dec09a731766ece4cf
dvavatsioulas <dvavatsioulas@gmail.com>
2019-12-12 12:06:46 +0200

    Add about text & add margin to FAQ page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,11 +8,14 @@ class About extends Component {
         <blockquote className="blockquote text-center homePageStyle">
           <h1 className="display-4">About us</h1>
           <p>
-            We are the best real estate site available and we will make sure<br/>
-            that you will find your perfect house or your perfect renter
+          Founded in 2019 by dynamic business leaders ("System of a Down"), HouseCat has already
+          helped more than 5,000 people <br/> to find their dream homes through the efforts
+          of more than 200 of our dedicated employees.<br/>
+          We are the best real estate site available and we will make sure
+          that you will find your perfect house or your perfect renter!
           </p>
           <footer className="blockquote-footer">
-            <cite title="Source Title">The Housecat team</cite>
+            <cite title="Source Title">The HouseCat team</cite>
           </footer>
         </blockquote>
       </React.Fragment>
